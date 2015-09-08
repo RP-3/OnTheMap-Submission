99c1ac3c734cb4ce5eac9b331c1b92b2683e7127
Rohan <sarith21@gmail.com>
2015-09-08 09:01:53 -0700

    Added static assets and gradient convenience class extension.

---
+++ OnTheMap-Submission/CAGradientLayer+Convenience.swift
@@ -0,0 +1,13 @@
+//
+//  CAGradientLayer+Convenience.swift
+//  OnTheMap-Submission
+//
+//  Created by Rohan Sarith Pethiyagoda on 8/09/2015.
+//  Copyright (c) 2015 RP3. All rights reserved.
+//
+
+import UIKit
+
+class CAGradientLayer_Convenience: NSObject {
+   
+}
